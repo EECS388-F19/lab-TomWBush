dc4f595efda575a77ef385b060097e0830085f79
zww <zww@umich.edu>
2019-09-09 14:38:40 -0400

    init

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from random import randint
+a = randint(1,100)
+b = randint(1,100)
+sum = a + b
+
+print("Tom Wang")
+print(a)
+print(b)
+print("Sum = " + str(sum))
+print("Average = " + str(sum/2.0))
